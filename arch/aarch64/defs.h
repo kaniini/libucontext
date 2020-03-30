fb27ad693bb6455d5262f86fdd3f8157cfbd211f
Ariadne Conill <ariadne@dereferenced.org>
2020-03-30 04:37:20 +0000

    aarch64: modernize

--- arch/aarch64/defs.h
+++ arch/aarch64/defs.h
@@ -1,8 +1,11 @@
 #ifndef __ARCH_AARCH64_DEFS_H
 #define __ARCH_AARCH64_DEFS_H
 
-#define REGSZ		8
-#define R0_OFFSET	184
+#define REG_SZ		(8)
+#define MCONTEXT_GREGS	(184)
+
+#define R0_OFFSET	REG_OFFSET(0)
+
 #define SP_OFFSET	432
 #define PC_OFFSET	440
 #define PSTATE_OFFSET	448
@@ -14,5 +17,7 @@
 #ifndef ESR_MAGIC
 # define ESR_MAGIC	0x45535201
 #endif
+
+#include "common-defs.h"
 
 #endif

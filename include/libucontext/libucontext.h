b497888eeeb7244a50e5479f1e12ba288b8dce3d
keengo99 <99380333+keengo99@users.noreply.github.com>
2022-05-16 22:25:54 +0800

    Update libucontext.h
    fix link bug with c++ project.

--- include/libucontext/libucontext.h
+++ include/libucontext/libucontext.h
@@ -3,10 +3,14 @@
 
 #include <stddef.h>
 #include <libucontext/bits.h>
-
+#ifdef  __cplusplus
+extern "C" {
+#endif
 int  libucontext_getcontext(libucontext_ucontext_t *);
 void libucontext_makecontext(libucontext_ucontext_t *, void (*)(), int, ...);
 int  libucontext_setcontext(const libucontext_ucontext_t *);
 int  libucontext_swapcontext(libucontext_ucontext_t *, const libucontext_ucontext_t *);
-
+#ifdef  __cplusplus
+}
+#endif
 #endif

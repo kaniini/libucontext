kaniini/libucontext

commit 9e5de65076690c8be681f529065870440606d028

libucontext: Add patch fixing return values of get/set/swapcontext
The arm ABI defines that the value in register r0 is used as the return
value of a function. To indicate success for get/set/swapcontext (return
value of 0) the register r0 must contain zero at the end of the function
call. Thus set r0 to zero and store it in the context. This context
is restored later and indicate successful execution, because r0 is 0.

The order registers are stored has changed so that only one additional
instruction (mov r0, #0) needs to be added to fix the return value
bug for get/set/swapcontext.

Signed-off-by: Volker Christian <me@vchrist.at>

arch/arm/swapcontext.S
@@ -16,12 +16,6 @@ ALIAS(swapcontext, libucontext_swapcontext)
 ALIAS(__swapcontext, libucontext_swapcontext)
 
 FUNC(libucontext_swapcontext)
-	/* copy all of the current registers into the ucontext structure */
-	add	r2, r0, #REG_OFFSET(0)
-	stmia	r2, {r0-r12}
-	str	r13, [r0,#REG_OFFSET(13)]
-	str	r14, [r0,#REG_OFFSET(15)]
-
 #ifndef FORCE_SOFT_FLOAT
 #ifndef FORCE_HARD_FLOAT
 	/* test for vfp magic number, copy to other ucontext */
@@ -42,6 +36,14 @@ FUNC(libucontext_swapcontext)
 1:
 #endif
 
+ 	/* copy all of the current registers into the ucontext structure */
+ 	str	r13, [r0,#REG_OFFSET(13)]
+ 	str	r14, [r0,#REG_OFFSET(15)]
+	add	r2, r0, #REG_OFFSET(0)
+	/* copy r0 with value 0 to indicate success (return value 0) */
+	mov r0, #0
+	stmia	r2, {r0-r12}
+	
 	/* load new registers from the second ucontext structure */
 	add	r14, r1, #REG_OFFSET(0)
 	ldmia	r14, {r0-r12}

commit 200b4e79a43963358c93c7c33b5b6c62b456849d

fix arm hard float code path broken by 9e5de65076

Makefile
@@ -44,8 +44,7 @@ else
 	CPPFLAGS += -Iarch/common/include
 endif
 
-# ARM hard float support is presently broken.
-FORCE_SOFT_FLOAT := yes
+FORCE_SOFT_FLOAT := no
 
 ifeq ($(FORCE_SOFT_FLOAT),yes)
         CPPFLAGS += -DFORCE_SOFT_FLOAT

arch/arm/swapcontext.S
@@ -16,13 +16,17 @@ ALIAS(swapcontext, libucontext_swapcontext)
 ALIAS(__swapcontext, libucontext_swapcontext)
 
 FUNC(libucontext_swapcontext)
+	/* copy all of the current registers into the ucontext structure */
+	str	r13, [r0,#REG_OFFSET(13)]
+	str	r14, [r0,#REG_OFFSET(15)]
+
 #ifndef FORCE_SOFT_FLOAT
 #ifndef FORCE_HARD_FLOAT
 	/* test for vfp magic number, copy to other ucontext */
 	ldr	r3, [r1, #VFP_MAGIC_OFFSET]
-	ldr	r4, =#0x56465001
+	ldr	r2, =#0x56465001
 	str	r3, [r0, #VFP_MAGIC_OFFSET]
-	cmp	r3, r4
+	cmp	r3, r2
 	bne	1f
 #endif
 	/* if vfp in use, save and restore d8-d15 */
@@ -36,9 +40,6 @@ FUNC(libucontext_swapcontext)
 1:
 #endif
 
- 	/* copy all of the current registers into the ucontext structure */
- 	str	r13, [r0,#REG_OFFSET(13)]
- 	str	r14, [r0,#REG_OFFSET(15)]
 	add	r2, r0, #REG_OFFSET(0)
 	/* copy r0 with value 0 to indicate success (return value 0) */
 	mov r0, #0
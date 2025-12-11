f6bcbe42843d2dcdf0befa35fe2a0749ae39e40c
Ariadne Conill <ariadne@ariadne.space>
2025-12-11 15:14:09 -0800

    common: work around glibc ucontext.h header bug on powerpc
    greg_t is not defined, despite it being defined everywhere else
    
    Signed-off-by: Ariadne Conill <ariadne@ariadne.space>

--- arch/common/include/libucontext/bits.h
+++ arch/common/include/libucontext/bits.h
@@ -5,6 +5,11 @@
 
 #include <ucontext.h>
 
+/* glibc on powerpc does not define greg_t */
+#if defined(__GLIBC__) && defined(__powerpc__)
+typedef unsigned long greg_t;
+#endif
+
 typedef greg_t libucontext_greg_t;
 typedef ucontext_t libucontext_ucontext_t;
 

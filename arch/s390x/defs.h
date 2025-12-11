3419d9a77712eb49092bcf4da4a5f49d84c0ae67
Ariadne Conill <ariadne@ariadne.space>
2025-12-10 17:16:29 -0800

    s390x: add hard-float support
    Signed-off-by: Ariadne Conill <ariadne@ariadne.space>

--- arch/s390x/defs.h
+++ arch/s390x/defs.h
@@ -3,12 +3,15 @@
 
 #define REG_SZ		(8)
 #define AREG_SZ		(4)
+#define FPREG_SZ	(8)
 
 #define MCONTEXT_GREGS	(56)
 #define MCONTEXT_AREGS	(184)
-#define MCONTEXT_FPREGS	(248)
+#define MCONTEXT_FPC	(248)
+#define MCONTEXT_FPREGS	(256)
 
 #define AREG_OFFSET(__reg)	(MCONTEXT_AREGS + ((__reg) * AREG_SZ))
+#define FPREG_OFFSET(__reg)	(MCONTEXT_FPREGS + ((__reg) * FPREG_SZ))
 
 #include "common-defs.h"
 
